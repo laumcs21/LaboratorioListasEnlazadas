c9399e1a405153ab60f85ecee74cc75f8126c3a6
Jefferson RodriguezV <jefferson.rodriguezv@uqvirtual.edu.co>
2025-03-31 22:24:37 -0500

    se agregan los ejercicios 2 y 4

---
+++ src/main/java/listas/enlazadas/Ejercicio4/App.java
@@ -0,0 +1,50 @@
+package listas.enlazadas.Ejercicio4;
+
+import listas.enlazadas.ListaSimple;
+import listas.enlazadas.Nodo;
+
+public class App {
+    
+
+    public static void main(String[] args) {
+
+        ListaSimple<Integer> lista = new ListaSimple<Integer>();
+
+        lista.addFirst(1);
+        lista.addFirst(2);
+        lista.addFirst(6);
+        lista.addFirst(4);
+        lista.addFirst(7);
+
+        ListaSimple<Integer> valoresImpares = obtenerValoresImpares(lista);
+
+        Nodo<Integer> aux = valoresImpares.getNodoPrimero();
+        while(aux != null){
+            System.out.println(aux.getDato());
+            aux = aux.getNodoSiguiente();
+        }
+
+
+        
+    }
+
+    public static ListaSimple<Integer> obtenerValoresImpares(ListaSimple<Integer> lista){
+        ListaSimple<Integer> resultado = new ListaSimple<>();
+        Nodo<Integer> aux = lista.getNodoPrimero();
+        int i = 0;
+
+        for(; aux != null; aux = aux.getNodoSiguiente(), i++){
+
+            int dato = aux.getDato();
+            
+            if (dato % 2 != 1){
+            continue;
+            }
+
+            resultado.addFirst(dato);
+            
+        }
+        return resultado;
+
+    }
+}
